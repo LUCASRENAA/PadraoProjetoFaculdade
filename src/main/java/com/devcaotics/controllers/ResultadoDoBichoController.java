e1bbfe60597d19f0d76be4f7f8bb29f10f68c2a7
LUCASRENAA_IFPE <lrms2@discente.ifpe.edu.br>
2022-08-09 21:54:46 -0300

    teste

--- src/main/java/com/devcaotics/controllers/ResultadoDoBichoController.java
+++ src/main/java/com/devcaotics/controllers/ResultadoDoBichoController.java
@@ -1,6 +1,5 @@
 package com.devcaotics.controllers;
 
-import java.net.http.HttpRequest;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.devcaotics.model.entities.Carro;
-import com.devcaotics.model.entities.Prato;
 import com.devcaotics.model.entities.ResultadoDoBicho;
 import com.devcaotics.model.repository.Facade;
 
@@ -25,11 +22,11 @@ public class ResultadoDoBichoController {
 	}
 	
 	@RequestMapping("/index")
-	public String index2(Model m) {
+	public String index20(Model m) {
 		List<ResultadoDoBicho> resultados = Facade.getCurrentInstance().readAll();
 		
 		m.addAttribute("resultados",resultados);
-		return "resultado/ultimoresultado";
+		return "resultado/";
 		
 	}
 	
